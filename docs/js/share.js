7ed99c6c22fcea68468a0cc098bbda38a9bc8af7
taka-sho <takasho530000@gmail.com>
2019-03-14 02:58:43 +0900

    w/ hash

--- docs/js/share.js
+++ docs/js/share.js
@@ -15,8 +15,8 @@
 $(document).ready(function () {
   $("a[href*='twitter.com']").click(function () {
     if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)) {
-      // location.href = 'twitter://post?message=Mr.KEIO CONTEST HP'
-      location.href = 'twitter://'
+      location.href = 'twitter://post?message=Mr.KEIO CONTEST HP'
+      // location.href = 'twitter://'
       setTimeout(function () {
         location.href = 'https://twitter.com/intent/tweet?url=asdf.com&text=テキスト&via=takasho53000&hashtags=#test&related=takasho53000'
         // open('https://twitter.com/intent/tweet?url=asdf.com&text=テキスト&via=takasho53000&hashtags=#test&related=takasho53000', '_blank')  

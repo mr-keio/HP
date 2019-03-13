6f35a5b30153f21381e3ac57b3281147e4f1f839
taka-sho <takasho530000@gmail.com>
2019-03-14 02:52:04 +0900

    twitter://

--- docs/js/share.js
+++ docs/js/share.js
@@ -15,15 +15,12 @@
 $(document).ready(function () {
   $("a[href*='twitter.com']").click(function () {
     if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)) {
-      try {
-        location.href = 'twitter://post?message=Mr.KEIO CONTEST HP'
-      } catch {
-        setTimeout(function () {
-          location.href = 'https://twitter.com/intent/tweet?url=asdf.com&text=テキスト&via=takasho53000&hashtags=#test&related=takasho53000'
-          // open('https://twitter.com/intent/tweet?url=asdf.com&text=テキスト&via=takasho53000&hashtags=#test&related=takasho53000', '_blank')  
-        }, 500)  
-      }
-      // location.href = 'twitter://'
+      // location.href = 'twitter://post?message=Mr.KEIO CONTEST HP'
+      location.href = 'twitter://'
+      setTimeout(function () {
+        location.href = 'https://twitter.com/intent/tweet?url=asdf.com&text=テキスト&via=takasho53000&hashtags=#test&related=takasho53000'
+        // open('https://twitter.com/intent/tweet?url=asdf.com&text=テキスト&via=takasho53000&hashtags=#test&related=takasho53000', '_blank')  
+      }, 500)
     } else {
       open('https://twitter.com/intent/tweet?url=asdf.com&text=テキスト&via=takasho53000&hashtags=#test&related=takasho53000', '_blank')
     }

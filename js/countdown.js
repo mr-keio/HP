94bd1fb8c47bfe7b8e50c882e77e0fdea91d1e08
taka-sho <takasho@keio.jp>
2019-03-22 19:50:05 +0900

    stronger countdown

--- js/countdown.js
+++ js/countdown.js
@@ -4,13 +4,12 @@ $(document).ready(function () {
       window.alert(e.text + 'をコピーしました')
   })
   setInterval(function () {
-    var ts = countdown(new Date(2019, 4, 20)).toString()
+    var ts = countdown(new Date(2019, 4, 20)).toHTML()
     var result = ts
-      .replace(/day/g, '日')
-      .replace(/hour/g, '時間')
-      .replace(/minute/g, '分')
-      .replace(/second/g, '秒')
-      .replace(/s/g, '')
+      .replace(/days|day/g, "<span>日</span>")
+      .replace(/hours|hour/g, "<span>時間</span>")
+      .replace(/minutes|minute/g, "<span>分</span>")
+      .replace(/seconds|second/g, "<span>秒</span>")
       .replace(/and/g, '')
       .replace(/,/g, '')
       .replace(/ /g, '')
